d825633a9a5ccc32db26c5b2aa3a7129f2061995
Luan Daniel <u21609170@tuks.co.za>
2023-05-15 14:43:33 +0200

    Merge pull request #2 from Informatics-370/EarlyBird
    Changed early bird in modelbuilder to be a lookup table for events

--- API/Model/Blacklist.cs
+++ API/Model/Blacklist.cs
@@ -1,11 +1,19 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace API.Model
 {
     public class Blacklist
     {
         [Key]
-        public int UserID { get; set; }
+        public int BlacklistID { get; set; }
+
+        [ForeignKey("UserID")]
+        public int? UserID { get; set; }
+        public User User { get; set; }
+
+        [MaxLength(50)]
+        public string Email { get; set; }
 
         [MaxLength(255)]
         public string Reason { get; set; }
