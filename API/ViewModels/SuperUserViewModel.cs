b0b82c36a6ed4b996e34c1aa65a47612a1ba44ca
Dihan de Bod <u21451193@tuks.co.za>
2023-07-07 10:17:02 +0200

    Did the last updates of superuser accounts as well as assigning roles and removing roles from accounts.

---
+++ API/ViewModels/SuperUserViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace API.ViewModels
+{
+    public class SuperUserViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string IDNumber { get; set; }
+    }
+}
