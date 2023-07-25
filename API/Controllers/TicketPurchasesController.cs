2f4bdd9600f1a473151d203c0a8724d25004f3ba
Marco McL <mclarenmarco998@gmail.com>
2023-07-25 11:54:21 +0200

    View the tickets of a user

---
+++ API/Controllers/TicketPurchasesController.cs
@@ -0,0 +1,59 @@
+﻿using API.Data;
+using API.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketPurchasesController : ControllerBase
+    {
+        private readonly MyDbContext _context;
+
+        public TicketPurchasesController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TicketPurchase>> PostTicketPurchase(TicketPurchase ticketPurchase)
+        {
+            _context.TicketPurchases.Add(ticketPurchase);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTicketPurchase), new { id = ticketPurchase.Id }, ticketPurchase);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TicketPurchase>> GetTicketPurchase(int id)
+        {
+            var ticketPurchase = await _context.TicketPurchases.FindAsync(id);
+
+            if (ticketPurchase == null)
+            {
+                return NotFound();
+            }
+
+            return ticketPurchase;
+        }
+
+        // New method to get all purchases for a user
+        [HttpGet("User/{userEmail}")]
+        public async Task<ActionResult<IEnumerable<TicketPurchase>>> GetPurchasesForUser(string userEmail)
+        {
+            var ticketPurchases = await _context.TicketPurchases
+                .Where(tp => tp.UserEmail == userEmail)
+                .ToListAsync();
+
+            if (ticketPurchases == null || ticketPurchases.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return ticketPurchases;
+        }
+
+    }
+}
