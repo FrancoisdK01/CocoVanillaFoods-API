f18c15e991622f563ad5d4ab7636bfdd398c9e8f
Dihan de Bod <u21451193@tuks.co.za>
2023-07-26 20:50:57 +0200

    Letterlik net die edit bygesit vir userinformation

--- API/Controllers/CustomersController.cs
+++ API/Controllers/CustomersController.cs
@@ -8,6 +8,9 @@ using Microsoft.EntityFrameworkCore;
 using API.Data;
 using API.Model;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using System.Data;
 
 namespace API.Controllers
 {
@@ -46,6 +49,70 @@ namespace API.Controllers
             {
                 return Ok(new { user });
             }
+        }
+    
+
+        [HttpPut("{id}")]
+        [Authorize(Roles = "Customer")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<IActionResult> PutEmployee(string id, Customer customer)
+        {
+            var customerDetailsBeforeUpdate = await _context.Customers.FindAsync(id);
+            var existingCustomer = await _context.Customers.FindAsync(id);
+            var existingUser = await _userManager.FindByIdAsync(existingCustomer.UserID);
+
+            if (existingCustomer == null)
+            {
+                return NotFound("There is no employee that has the ID you provided");
+            }
+            else
+            {
+                existingCustomer.Id = existingCustomer.Id;
+
+                existingCustomer.First_Name = customer.First_Name;
+                existingCustomer.Last_Name = customer.Last_Name;
+                existingCustomer.Email = customer.Email;
+                existingCustomer.PhoneNumber = customer.PhoneNumber;
+                existingCustomer.ID_Number = customer.ID_Number;
+                existingCustomer.Title = customer.Title;
+                existingCustomer.Gender = customer.Gender;
+
+                var savedcustomerChanges = await _context.SaveChangesAsync();
+                if (savedcustomerChanges > 0)
+                {
+                    if (existingUser == null)
+                    {
+                        // Revert Customer details to their original state
+                        existingCustomer.First_Name = customerDetailsBeforeUpdate.First_Name;
+                        existingCustomer.Last_Name = customerDetailsBeforeUpdate.Last_Name;
+                        existingCustomer.Email = customerDetailsBeforeUpdate.Email;
+                        existingCustomer.PhoneNumber = customerDetailsBeforeUpdate.PhoneNumber;
+                        existingCustomer.ID_Number = customerDetailsBeforeUpdate.ID_Number;
+                        existingCustomer.Title = customerDetailsBeforeUpdate.Title;
+                        existingCustomer.Gender = customerDetailsBeforeUpdate.Gender;
+
+                        await _context.SaveChangesAsync();
+
+                        return NotFound("The user account linked to the Customer account doesn't exist");
+                    }
+                    else
+                    {
+                        existingUser.UserName = customer.First_Name;
+                        existingUser.Email = customer.Email;
+                        existingUser.DisplayName = customer.First_Name;
+
+                        try
+                        {
+                            await _context.SaveChangesAsync();
+                        }
+                        catch (Exception ex)
+                        {
+                            return BadRequest(ex.Message);
+                        }
+                    }
+                }
+            }
+            return Ok(existingCustomer);
         }
 
         // DELETE: api/Customers/5
