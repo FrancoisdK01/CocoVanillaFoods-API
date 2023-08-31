a129cb5f730d756bde0b32286df993cd5769f40f
Dihan de Bod <u21451193@tuks.co.za>
2023-08-31 13:52:39 +0200

    Added audit log

---
+++ API/ViewModels/AuditLogViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace API.ViewModels
+{
+    public class AuditLogViewModel
+    {
+        public string email { get; set; }
+        public string buttonClicked { get; set; }
+        public int? quantity { get; set; }
+
+    }
+}
